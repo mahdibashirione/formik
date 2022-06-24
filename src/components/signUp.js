448d8cd0997799321e3f153f78878697416de982
mahdi bashiri <mahdi94250@gmail.com>
2022-06-24 20:55:18 +0430

    add hook useFormik in component signUp

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,37 +1,50 @@
-
+import { useFormik } from "formik";
 
 const SignUp = () => {
+
+  const formik = useFormik({
+    initialValues: { name: "", email: "", password: "" },
+  })
+
+  console.log(formik)
+
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <form className="bg-purple-400 p-4 rounded-lg max-w-[350px] w-full">
+      <form className="border p-4 rounded-lg max-w-[350px] w-full">
         <div className="flex flex-col items-start gap-y-2 mt-4">
-          <lable className="text-white font-sans" for="name">Name</lable>
+          <lable className="text-slate-800 font-sans" for="name">Name</lable>
           <input
+            onChange={formik.handleChange}
             type={"text"}
             name="name"
             id="name"
-            className="w-full px-4 py-3 bg-white rounded-lg"
+            className="w-full px-4 py-3 bg-gray-200 rounded-lg"
             placeholder="alireza..."
+            value={formik.values.name}
           />
         </div>
         <div className="flex flex-col items-start gap-y-2 mt-4">
-          <lable className="text-white font-sans" for="email">Email</lable>
+          <lable className="text-slate-800 font-sans" for="email">Email</lable>
           <input
+            onChange={formik.handleChange}
             type={"text"}
             name="email"
             id="email"
-            className="w-full px-4 py-3 bg-white rounded-lg"
+            className="w-full px-4 py-3 bg-gray-200 rounded-lg"
             placeholder="ali@gmail.com..."
+            value={formik.values.email}
           />
         </div>
         <div className="flex flex-col items-start gap-y-2 mt-4">
-          <lable className="text-white font-sans" for="password">Password</lable>
+          <lable className="text-slate-800 font-sans" for="password">Password</lable>
           <input
+            onChange={formik.handleChange}
             type={"text"}
             name="password"
             id="password"
-            className="w-full px-4 py-3 bg-white rounded-lg"
+            className="w-full px-4 py-3 bg-gray-200 rounded-lg"
             placeholder="A562cxs5a..."
+            value={formik.values.password}
           />
         </div>
         <button className="w-full py-3 bg-blue-500 text-white font-sans mt-8 rounded-lg">sign Up</button>
